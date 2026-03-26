f1a44fd9e83c45773c9b2b06926b9b9ba02b460f
Christian <licorice929@gmail.com>
2026-03-26 23:10:20 +0800

    Fixed the App.tsx, installed tailwindcss, and stuff (functional now)

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./tailwind.css";
+import "./assets/styles/tailwind.css";
 import App from "./app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
